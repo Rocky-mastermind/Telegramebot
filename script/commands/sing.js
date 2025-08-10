bc776f0b5ecce2267fbb349d8e3163c6b1598612
Aryan <aryanchowdhury439@yahoo.com>
2025-08-10 12:39:02 +0600

    Update sing.js

--- script/commands/sing.js
+++ script/commands/sing.js
@@ -50,8 +50,8 @@ module.exports = {
       const videoId = topResult.videoId;
       const songTitle = topResult.title;
 
-      const apiKey = global.config.youtubeApiKey;
-      const apiUrl = `${global.api.sing}/youtube?id=${videoId}&type=${downloadType}&apikey=${apiKey}`;
+      const apiKey = "itzaryan";
+      const apiUrl = `https://xyz-nix.vercel.app/aryan/youtube?id=${videoId}&type=${downloadType}&apikey=${apiKey}`;
 
       await bot.editMessageText(`⏳ Found "${songTitle}". Downloading...`, {
         chat_id: msg.chat.id,
